d012a733025f53a6b0dce5298f9513b561b1aabc
Abdalla Hussein <a.hussein1934@gmail.com>
2024-09-28 00:54:15 +0100

    added logout feature after 1 hour idle

--- authentication/frontend/src/utility/auth.js
+++ authentication/frontend/src/utility/auth.js
@@ -1,10 +1,33 @@
 import { redirect } from 'react-router-dom';
+
+// working out duration since login 
+export const getTokenDuration = () => {
+    const storedExpirationDate = localStorage.getItem("expiration");
+    const expirationDate = new Date(storedExpirationDate);
+    const now = new Date();
+    const duration = expirationDate.getTime() - now.getTime();
+    
+    return duration;
+}
 export const getAuthToken = () => {
     const token = localStorage.getItem('token');
+
+
+    if (!token) {
+        return null;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    if (tokenDuration < 0) {
+        return 'EXPIRED'
+    }
+
     return token;
 }
 
 export const tokenLoader = () => {
+    console.log(getAuthToken());
     return getAuthToken();
 }
 
@@ -15,4 +38,6 @@ export const checkAuthTokenLoader = () => {
     if (!token) {
         return redirect('/auth');
     }
+    // loader functions do not work as intended when you return undefined as result so therefor you must return null as final result. 
+    return null
 }
