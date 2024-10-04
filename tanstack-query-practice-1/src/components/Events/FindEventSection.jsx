6c46b9abee182fdb9a66624055428b71b45a37e8
Abdalla Hussein <a.hussein1934@gmail.com>
2024-10-04 06:48:54 +0100

    Added search query

--- tanstack-query-practice-1/src/components/Events/FindEventSection.jsx
+++ tanstack-query-practice-1/src/components/Events/FindEventSection.jsx
@@ -8,7 +8,7 @@ import EventItem from "./EventItem";
 export default function FindEventSection() {
   const searchElement = useRef();
   const [searchTerm, setSearchTerm] = useState("");
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["events", { search: searchTerm }],
     queryFn: (({signal}) => fetchEvents({signal,searchTerm})),
   });
@@ -16,11 +16,12 @@ export default function FindEventSection() {
   function handleSubmit(event) {
     event.preventDefault();
     setSearchTerm(searchElement.current.value);
+    enable: searchTerm !== undefined;
   }
 
   let content = <p>Please enter a search term and to find events</p>;
 
-  if (isPending) {
+  if (isLoading) {
     content = <LoadingIndicator />;
   }
 
--- tanstack-query-practice-1/src/util/http.js
+++ tanstack-query-practice-1/src/util/http.js
@@ -1,21 +1,57 @@
 
-export const fetchEvents = async ({signal, searchTerm}) => {
+export const fetchEvents = async ({ signal, searchTerm }) => {
     let url = "http://localhost:3000/events";
     if (searchTerm) {
-        url += '?search=' + searchTerm ;
+        url += `?search=${searchTerm}`;
     }
+
     const response = await fetch(url, signal);
     if (!response.ok) {
-        const error = new Error("An error occurred whiled feching data!");
+        const error = new Error('An Error has occurred, could not fetch data!');
         error.code = error.status;
         error.info = await response.json();
         throw error;
     }
+
     const { events } = await response.json();
     return events;
 }
 
+// export const fetchEvents = async ({ signal, searchTerm }) => {
+  
+//     let url = "http://localhost:3000/events";
+//     if (searchTerm) {
+//         url += '?search=' + searchTerm ;
+//     }
+//     const response = await fetch(url, signal);
+//     if (!response.ok) {
+//         const error = new Error("An error occurred whiled feching data!");
+//         error.code = error.status;
+//         error.info = await response.json();
+//         throw error;
+//     }
+//     const { events } = await response.json();
+//     return events;
+// }
 
+
+// export const fetchEvents = async({ signal, searchTerm }) => {
+//     let url = "http://localhost:3000/events";
+//     if (searchTerm) {
+//         url += "?search=" + searchTerm;
+//     }
+
+//     const response = await fetch(url, signal);
+//     if (!response.ok) {
+//         const error = new Error('An Error has occured while fetching data!');
+//         error.code = error.status;
+//         error.info = await response.json();
+//         throw error;
+//     }
+
+//     const { events } = await response.json();
+//     return events;
+// }
 
 // export const fetchEvents = async ({searchQuery}) => {
 //     let url = 'http://localhost:3000/events';
